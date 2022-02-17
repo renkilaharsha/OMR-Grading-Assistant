35eb86ad5cb74fb111c458e4d8896fd7316b507e
hayesall <alexander@batflyer.net>
2022-02-16 20:08:34 -0500

    ✨ Add non-maximum suppression, return corners
    `.find_corners()` method returns a list of corner objects now. This
    might be improved by returning vectors, but this is probably fine for
    now.

--- harris/harris.py
+++ harris/harris.py
@@ -41,6 +41,25 @@ Wikipedia highlights the five main steps:
 4. Harris response calculation
 5. Non-maximum suppression
 
+Basic Usage
+-----------
+
+This is implemented as a class that implements a ``find_corners``
+method. The ``find_corners`` method takes a grayscale image and
+returns a list of ``Corner`` objects.
+
+.. code-block:: python
+
+    from harris.harris import HarrisCornerDetector
+    from PIL import Image
+    import numpy as np
+
+    im = np.array(Image.open("docs/book_corners1.png"))
+
+    hcd = HarrisCornerDetector(alpha=0.04, threshold=30_000)
+
+    hcd.find_corners(im)
+
 References
 ----------
 
@@ -63,7 +82,6 @@ References
 """
 
 from functools import total_ordering
-from math import inf
 
 import numpy as np
 from PIL import Image
@@ -74,7 +92,7 @@ from scipy.ndimage import convolve1d
 def log_image(image, name: str):
     # Save a copy of a grayscale image.
     im = Image.fromarray(image.astype(np.uint8))
-    im.save(name + ".jpg")
+    im.save(name + ".png")
 
 
 @total_ordering
@@ -121,11 +139,9 @@ class Corner:
 
 class HarrisCornerDetector:
     """Harris Corner Detector object
-
-    - Images must be grayscale.
     """
 
-    def __init__(self, alpha=0.05, threshold=20000):
+    def __init__(self, alpha=0.05, threshold=25000):
         """Initialize object
 
         Arguments:
@@ -135,11 +151,32 @@ class HarrisCornerDetector:
         self.alpha = alpha
         self.threshold = threshold
 
-    def find_corners(self, image, *, border=20, n_corners=inf):
-        """Apply the Harris detector to find corners in an image."""
+    @staticmethod
+    def is_local_max(Q, u, v):
+        """Return True if Q[u, v] is a local maximum"""
+        return Q[u, v] == np.max(Q[u-1:u+2, v-1:v+2])
 
-        if isinstance(image, JpegImageFile):
-            image = np.array(image)
+    def collect_corners(self, Q, border=2):
+        corner_list = []
+        h, w = Q.shape
+
+        for u in range(border, h - border):
+            for v in range(border, w - border):
+                q = Q[u, v]
+                if q > self.threshold and self.is_local_max(Q, u, v):
+                    corner_list.append(Corner(u, v, q))
+
+        return corner_list
+
+    def find_corners(self, image):
+        """Apply the Harris detector to find corners in an image.
+
+        Arguments:
+            image: A grayscale image as a numpy array.
+
+        Returns:
+            A list of Corner objects.
+        """
 
         imf = image.astype(np.float64)
 
@@ -156,29 +193,27 @@ class HarrisCornerDetector:
         B = convolve1d(convolve1d(Iy ** 2, bfilt, axis=1), bfilt, axis=0)
         C = convolve1d(convolve1d(Ix * Iy, bfilt, axis=1), bfilt, axis=0)
 
-        log_image(A, "out1")
-        log_image(B, "out2")
-        log_image(C, "out3")
-
         # "Make the Corner Response Function (CRF)"
 
         h, w = image.shape
         Q = np.zeros((h, w))
 
-        for v in range(h):
-            for u in range(w):
+        for u in range(h):
+            for v in range(w):
 
-                a = A[v, u]
-                b = B[v, u]
-                c = C[v, u]
+                a = A[u, v]
+                b = B[u, v]
+                c = C[u, v]
 
                 det = (a * b) - (c * c)
                 trace = a + b
-                Q[v, u] = det - self.alpha * (trace * trace)
 
-        log_image(Q, "out4")
+                Q[u, v] = det - (self.alpha * (trace ** 2))
 
-        return A, B
+        # Collect the corners and perform non-maximal suppression.
+        corners = self.collect_corners(Q)
+
+        return corners
 
     def __repr__(self):
         _attributes = ["alpha", "threshold"]
