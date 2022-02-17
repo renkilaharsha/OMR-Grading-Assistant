2a2fc005d54b1fc88d46e6728af257f089a36057
hayesall <alexander@batflyer.net>
2022-02-16 20:09:43 -0500

    📝 Add `harris_demo.py` full example
    This prints the final set of coordinates from the demo image

--- harris_demo.py
+++ harris_demo.py
@@ -2,13 +2,21 @@
 # Computer Vision - CSCI-B 657 - Spring 2022
 # MIT License
 
+"""
+Short demo for working with the Harris Corner Detector.
+
+- For `a-3`, threshold=10_000_000 seems reasonable
+- For `book_corners1.png`, the default threshold is fine
+"""
+
 from harris.harris import HarrisCornerDetector
 from PIL import Image
 import numpy as np
 
-im = Image.open("docs/corner_demo_2.png")
-im2 = np.array(im)
+im = np.array(Image.open("docs/book_corners1.png"))
 
-hcd = HarrisCornerDetector(alpha=0.04)
+hcd = HarrisCornerDetector(alpha=0.04, threshold=30000)
 
-corners = hcd.find_corners(im2)
+corners = hcd.find_corners(im)
+
+print(corners)
