fdf0a05c09ca18f0dc746ce26be90c754f1e444f
hayesall <alexander@batflyer.net>
2022-02-20 19:42:29 -0500

    ✨ Add obfuscation to the inject/extract steps

--- inject.py
+++ inject.py
@@ -1,54 +1,67 @@
-# Discussion with Alexander and Ajinkya
+# Computer Vision - CSCI-B 657 - Spring 2022
 
 """
-Let's think through the injection portion first.
+Inject secret solutions into an image. 😈😈😈
 """
 
-from PIL import ImageOps
-import matplotlib.pyplot as plt
-import numpy as np
+import argparse
 from PIL import Image
-
-f = open("test-images/a-27_groundtruth.txt",'r')
-list_all_ans = []
-
-dict_options = {'A':0,'B':1,'C':2,'D':3,'E':4}
-
-for i in range(85):
-    content = f.readline()
-    list_qs = content.strip().split(" ")
-    list_all_ans.append(list_qs)
+from PIL import ImageOps
+import numpy as np
 
 
-print(list_all_ans)
-
-list_encoded = []
-for i in list_all_ans:
-    list_chars= [char for char in i[1]]
-    print(list_chars)
-    list_encoded.append([dict_options[char] for char in list_chars])
-
-np_arr_mcq = np.ones((5, 85))
-
-for idx1, chars in enumerate(list_encoded):
-    for char in chars:
-         np_arr_mcq[char, idx1] = 200
+def get_answer_file_content(file_location):
+    with open(file_location, "r") as fh:
+        data_raw = fh.read().splitlines()
+    return [line.strip("x").strip(" ").split() for line in data_raw]
 
 
-blank_form_img = Image.open("test-images/blank_form.jpg")
-gray_image = ImageOps.grayscale(blank_form_img)
-np_form = np.array(gray_image)
+def encode_solution_vector(input_data):
 
-# Let's roll the array ahead by 17
-np_array_mcq_rolled = np.roll(np_arr_mcq, 17)
+    dict_options = {'A':0,'B':1,'C':2,'D':3,'E':4}
 
-# Insert an expanded version of array into the form.
-np_form[400:400+np_arr_mcq.shape[0] * 4, 400:400 + np_arr_mcq.shape[1] * 4] = np.repeat(np.repeat(np.repeat(np.repeat(np_array_mcq_rolled, 2, axis=0), 2, axis=1), 2, axis=0), 2, axis=1)
+    list_encoded = []
 
-# Let's insert an additional pixel at 400 - 4, 400 - 4
-np_form[392:396, 392:396] = 0
-np_form[396:400, 396:400] = 0
+    for i in input_data:
+        list_chars= [char for char in i[1]]
+        list_encoded.append([dict_options[char] for char in list_chars])
 
-# We'll convert the numpy array back into a form and we'll save it.
+    # Convert the vector into a numpy array
+    solution_vector = np.zeros((5, 85))
+    for idx1, chars in enumerate(list_encoded):
+        for char in chars:
+            solution_vector[char, idx1] = 200
 
-Image.fromarray(np_form).save("injected_images.jpg")
+    return solution_vector
+
+
+def forward(vec):
+    """Roll vectors in two dimensions to obfuscate the solutions."""
+    return np.roll(np.roll(vec, 1, 0), 2, 1)
+
+
+if __name__ == "__main__":
+
+    PARSER = argparse.ArgumentParser(description='Inject the solutions into an image.')
+    PARSER.add_argument("image", help="The image to inject the solutions into.")
+    PARSER.add_argument("solutions", help="The solutions to inject.")
+    PARSER.add_argument("output", type=str, default="injected.jpg", help="Output image file.")
+    ARGS = PARSER.parse_args()
+
+    list_all_ans = get_answer_file_content(ARGS.solutions)
+
+    vec = encode_solution_vector(list_all_ans)
+
+    vec_encode = forward(vec)
+
+    im = np.array(ImageOps.grayscale(Image.open(ARGS.image)))
+
+    # Insert an expanded version of array into the form.
+    im[400:400+vec.shape[0] * 4, 400:400 + vec.shape[1] * 4] = np.repeat(np.repeat(np.repeat(np.repeat(vec_encode, 2, axis=0), 2, axis=1), 2, axis=0), 2, axis=1)
+
+    # Let's insert an additional pixel at 400 - 4, 400 - 4
+    im[392:396, 392:396] = 0
+    im[396:400, 396:400] = 0
+
+    # Convert the numpy array back into a form and we'll save it.
+    Image.fromarray(im).save(ARGS.output)
